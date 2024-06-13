3a90bd66fe33bd3b84aea72f520ad8595d22f37d
Serge-35 <166376730+Serge-35@users.noreply.github.com>
2024-06-13 11:18:14 +0300

    Add files via upload

--- modul8.1.py
+++ modul8.1.py
@@ -1,8 +1,10 @@
 def add_everything_up(a, b):
     try:
-        print(a + b)
+        result = a + b
     except TypeError:
-        print(str(a) + str(b))
+        result = str(a) + str(b)
+    return print(result)
+
 
 add_everything_up(123.456, 'строка')
 add_everything_up('яблоко', 4215)
