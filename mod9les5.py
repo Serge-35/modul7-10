0ed6a96d22f5f65578179898d3d94128f74e4f4c
Serge-35 <166376730+Serge-35@users.noreply.github.com>
2024-07-05 15:36:37 +0300

    Домашнее задание по теме "Декораторы" - Доработан

--- mod9les5.py
+++ mod9les5.py
@@ -1,10 +1,11 @@
 def is_prime(func):
     def wrapper(*args):
         result = func(*args)
-        if type(result) is int:
-            print('Простое')
-        else:
-            print('Составное')
+        for i in range(2, int(result ** 0.5) + 1):
+            if result % i == 0:
+                print('Составное')
+                return result
+        print('Простое')
         return result
     return wrapper
 
@@ -13,5 +14,9 @@ def sum_three(a, b, c):
     result = a + b + c
     return result
 
+
 sum1 = sum_three(2, 3, 6)
 print(sum1)
+sum2 = sum_three(3, 3, 6)
+print(sum2)
+
